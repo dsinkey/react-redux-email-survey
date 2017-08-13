9026ef26006468e6a998a7211de93044aca337a8
Dustin <dustinsinkey1@gmail.com>
2017-08-12 17:30:00 -0700

    Added production.env variables

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+// key.js - figure what credentials to return
+if (process.ENV.NODE_ENV === 'production') {
+    // return the production keys
+    module.exports = require('./prod');
+} else {
+    // we are in the developement - return the dev keys
+    module.exports = require('./dev');
+}
