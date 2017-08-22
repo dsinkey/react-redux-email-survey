124f3abeefab5f4efa17440a0e1a8b4e190ca738
Dustin <dustinsinkey1@gmail.com>
2017-08-21 19:24:30 -0700

    Added Landing Page Component and new route redirect on the backend for when logging out

---
+++ client/src/components/Landing.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Landing = () => {
+    return (
+        <div style={{textAlign: 'center'}}>
+            <h1>
+                Email Survey
+            </h1>
+            Collect fedback from users
+        </div>
+    );
+};
+
+export default Landing;
