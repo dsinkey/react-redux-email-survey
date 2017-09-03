40b7ba5a9c02347546f3e7549daa596c177ea435
Dustin <dustinsinkey1@gmail.com>
2017-09-02 18:18:51 -0700

    Added components for surveyFormReview and moved formFields into it's ownm file

---
+++ client/src/components/surveys/formFields.js
@@ -0,0 +1,10 @@
+export default [
+    {label: "Survey Title",
+    name: "title"},
+    {label: "Subject Line",
+    name: "subject"},
+    {label: "Email Body",
+    name: "email"},
+    {label: "Recipent List",
+    name: "emails"}
+];
