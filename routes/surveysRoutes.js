dde5d7b236880f5d445de46833aceb06e2672355
Dustin <dustinsinkey1@gmail.com>
2017-09-03 19:50:40 -0700

    Added lodash and used it to condense down results from SendGrid, and changed email template

--- routes/surveysRoutes.js
+++ routes/surveysRoutes.js
@@ -1,3 +1,6 @@
+const _ = require('lodash');
+const Path = require('path-parser');
+const { URL } = require('url');
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
@@ -12,7 +15,24 @@ module.exports = app => {
     });
 
     app.get('/api/surveys/webhooks', (req, res) => {
-        console.log(req.body);
+        const p = new Path('/api/surveys/:surveyId/:choice');
+        
+        const events = _.map(req.body, event => {
+            const pathname = new URL(event.url).pathname;
+            const match = p.test(pathname);
+            if (match) {
+                return {
+                    email: event.email,
+                    surveyId: match.surveyId,
+                    choice: match.choice,
+                };
+            }
+        });
+
+        const compactEvents = _.compact(events);
+        const uniqueEvent = _.uniqBy(compactEvents, 'email', 'surveyId');
+
+        console.log(uniqueEvent);
         res.send({});
     });
 
@@ -23,9 +43,11 @@ module.exports = app => {
             title,
             subject,
             body,
-            recipients: recipients.split(',').map(email => ({email: email.trim() })),
+            recipients: recipients
+                .split(',')
+                .map(email => ({ email: email.trim() })),
             _user: req.user.id,
-            dateSent: Date.now()
+            dateSent: Date.now(),
         });
 
         // Send email here
